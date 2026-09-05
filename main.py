0e6f8607b98a74a89a0a8dcf0804ff2c7a617828
SlawkoPL <slawekworek80@gmail.com>
2026-09-05 13:02:41 +0200

    Dodano gui/Added gui

--- main.py
+++ main.py
@@ -1,40 +1,53 @@
-print("Analizator tekstu:")
+import tkinter as tk
+
+# Main program
 
 def analyze():
-    text = input("Podaj tekst: ")
-    a = "!@#$%^&*()-=_+/*.,:;<>""''"
-    b = "1234567890"
+    text = text_input.get("1.0", "end-1c").replace("\r", "")
 
-    chars = 0
+    chars = len(text)
     special_chars = 0
-    spaces =0
+    spaces = 0
     lowercases = 0
     uppercases = 0
     numbers = 0
 
     for i in range(len(text)):   
-        if text[i] == " ":
+        if text[i].isspace():
             spaces +=1
-            pass
+        elif text[i].islower():
+            lowercases += 1
+        elif text[i].isupper():
+            uppercases += 1    
+        elif text[i].isdigit():
+            numbers += 1
         else:
-            if text[i].islower():
-                lowercases += 1
-            elif text[i].isupper():
-                uppercases += 1    
-            chars += text.count(text) 
-            for char in a:
-                if char == text[i]:
-                    special_chars += 1
-            for num in b:
-                if num == text[i]:
-                    numbers += 1        
+            special_chars += 1         
 
-    print(f"Łącznie znaków w tekscie: {chars+spaces}")                        
-    print(f"Litery: {chars-special_chars-numbers}")
-    print(f"Znaki specjalne: {special_chars}")
-    print(f"Duze litery: {uppercases}")
-    print(f"Male litery: {lowercases}")
-    print(f"Liczby: {numbers}")
-    print(f"Spacje: {spaces}")
+    result = f"""📊 WYNIKI ANALIZY TEKSTU/RESULTS:
+        ----------------------------------------
+        ● Łącznie wszystkich znaków/All chars: {chars}
+        ● Wszystkie litery/All letters: {uppercases+lowercases}
+        └ Małe litery/Lowercases: {lowercases}
+        └ Duże litery/Uppercases: {uppercases}
+        ● Liczby (cyfry)/Numbers: {numbers}
+        ● Znaki specjalne/Special chars: {special_chars}
+        ● Spacje i entery/Spaces + enters: {spaces}""" 
 
-analyze()        
+    result_text.set(result)    
+
+# GUI initialization
+window = tk.Tk()
+window.geometry("700x600")
+window.title("Text analyzer - analizator tekstu")
+
+info = tk.Label(window, text="Wprowadz tekst do analizy/Input text for analyze")
+text_input = tk.Text(window)
+analyze_button = tk.Button(window, text="Analyze", command=analyze)
+result_text = tk.StringVar(window)
+result_show = tk.Label(window, textvariable=result_text)
+info.pack()
+text_input.pack()
+analyze_button.pack()
+result_show.pack()
+window.mainloop()
